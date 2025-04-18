3c4e38ad5687ee534ab0b0b5af891a8492e19c99
Tati <tatianamedran@gmail.com>
2025-04-18 16:09:19 -0300

    CharacterMovement
    WASD, game object, rigiody 2d, box collider 2d.

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
 
-public class CharacterMovement
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterMovement : MonoBehaviour
 {
-    
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = direction * speed;
+    }
 }
+
